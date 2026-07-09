5c0de1599a5a1472514bfec0dd2fd953e290725e
Bangsano <sanoman984@gmail.com>
2026-07-09 12:41:16 +0800

    pqkamzajqh

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@
 BOLD='\033[1m'; BLUE='\033[0;34m'; RED='\033[0;31m'; GREEN='\033[0;32m'; YELLOW='\033[0;33m'; NC='\033[0m'
 
 run_remote_script() {
-    bash <(curl -s https://raw.githubusercontent.com/Bangsano/themeinstaller/main/install.sh)
+    bash <(curl -s https://cdn.jsdelivr.net/gh/Bangsano/themeinstaller@main/install.sh)
 }
 
 usage() {
